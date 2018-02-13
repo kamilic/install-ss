ef77ce9acd8539dcf9e62cb6268733d6a5facfa0
wangjin <wangjin@mama.cn>
2018-02-13 16:26:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# install-ss
+
+一个简单的 ss 安装脚本。
+
+
+
+### 使用方式
+
+```bash
+git clone https://github.com/kamilic/install-ss.git install-ss
+cd install-ss
+chmod +x install-ss.sh
+./install-ss.sh 169.32.53.22 mypassword # ./install-ss.sh <你的服务器 ip> <你的密码>
+```
+
+
+
---
+++ install-ss.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+if [ ! -n "$1" ]
+then
+        echo "parameter 1 is not defined."
+        exit 1;
+fi
+
+if [ ! -n "$2" ]
+then
+        echo "parameter 2 is not defined."
+        exit 1;
+fi
+
+jsonText='{"server":"'$1'","server_port":8388,"local_address":"127.0.0.1","local_port":1080,"password":"'$2'","timeout":500,"method":"aes-256-cfb"}'
+
+echo "Writing config files...."
+
+if [ -d /home/ss ]
+then
+    echo "dir /home/ss exists."
+    cd /home/ss
+else
+    echo "dir /home/ss doesn't exist."
+    cd /home
+    echo "create dir /home/ss"
+    mkdir /ss
+fi
+
+cd /home/ss
+echo "$jsonText" | cat > ss-config.json
+
+echo "Writing config files.... done."
+
+echo "installing shadowsocks.... (1/4)"
+apt-get update
+echo "installing shadowsocks... (2/4)"
+apt-get install -y python-pip
+echo "installing shadowsocks... (3/4)"
+pip install --upgrade pip
+echo "installing shadowsocks... (4/4)"
+pip install shadowsocks
+
+echo "installing shadowsocks... done"
+
+echo "============================================"
+echo "using following instructions to running ssserver"
+echo "ssserver -c /home/ss/ss-config.json -d start"
+echo "============================================"
+echo "enjoy!"
